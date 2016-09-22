9a15270a7375d34f09c2539f7ec3cb3d95f4ed39
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-09-22 11:35:16 -0400

    stop watch layout

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// alert('hey dan!');
+/*
+Set up your files as such, this is a good programming practice.
+(function(){
+  "use strict";
+
+
+}());
+*/
+
+(function(){
+  "use strict";
+
+
+
+
+
+}());
