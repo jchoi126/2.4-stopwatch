af98d53a989cac4a9c3915b754fe676d0005eb85
Johnnie Choi <johnniechoi@Johnnies-MacBook-Pro.local>
2016-09-23 13:31:12 -0400

    first push of the day

--- js/main.js
+++ js/main.js
@@ -27,56 +27,53 @@ Set up your files as such, this is a good programming practice.
 // timeOnPage.addEventListener("load",timeHandler)
 
 
-var colorHover = document.getElementById('hover-time');
 
+
+  var colorHover = document.getElementById('hover-time');
   var hours = document.querySelector('.elapsed-time .hours');
   var minutes = document.querySelector('.elapsed-time .minutes');
   var seconds = document.querySelector('.elapsed-time .seconds');
 
-  console.log(seconds);
-
-  var startTime;
-  var currentText;
-
+  // var startTime;
+  // var currentText;
 
   function colorHoverHandler(){
-    console.log('You clicked the button');
-    startTime = new Date();
-    toggleButton();
-
-
+    console.log();
+    // toggleButton();
     window.setInterval(logElapsedTime, 1000);
   }
 
   function logElapsedTime(){
     var currentTime = new Date();
-    var elapsedTime = currentTime ;
-    var elasped = millisecondsToInterval(elapsedTime);
-
-    displayInterval(elasped);
+    // var elapsedTime = currentTime ;
+    var elapsed = millisecondsToInterval(currentTime);
+    console.log(currentTime);
+    // console.log(60/currentTime);      Trying to get the percentages of a minute for time.
+    displayInterval(elapsed);
   }
 
-  function millisecondsToInterval(ms){
-    var msLeft = ms;
+/*
+Found get minutes command via following website.
+http://www.w3schools.com/jsref/jsref_getminutes.asp
+*/
+  var minutePercentage = function myFunction(number) {
+      var d = new Date();
+      var n = d.getMinutes()/60*100;
+      console.log(n);
+  }
+window.setInterval(minutePercentage, 1000);
 
-    var msInHour = 60000 * 60;
+
+  function millisecondsToInterval(ms){ //1000
+    var msLeft = ms; // => 1000
+    var msInHour = 60000 * 60; // 3,600,000
     var msInMinute = 60000;
     var msInSecond = 1000;
-
-    // We can devide two numbers using a forward slash
-    // We can use Math.floor() to chop off decimal values after doing math
-    var hourCount = Math.floor(msLeft / msInHour);
-
-    /*
-    Besides regular division we can do modulo division which returns the remainder
-    */
-    msLeft = msLeft % msInHour;
-
+    var hourCount = Math.floor(msLeft / msInHour); // => 26
+    msLeft = msLeft % msInHour;  // => 1000 / 3,600,000 =
     var minuteCount = Math.floor(msLeft / msInMinute);
     msLeft = msLeft % msInMinute;
-
     var secondCount = Math.floor(msLeft / msInSecond);
-
     return [hourCount, minuteCount, secondCount];
   }
 
@@ -86,12 +83,30 @@ var colorHover = document.getElementById('hover-time');
     seconds.textContent = ("0" + interval[2]).slice(-2);
   }
 
-  function toggleButton(){
-    currentText = colorHover.textContent;
-  }
+  // function toggleButton(){
+  //   currentText = colorHover.textContent;
+  // }
+
+logElapsedTime();
+
+  window.setInterval(logElapsedTime, 1000);
+
 
   colorHover.addEventListener('mouseover', colorHoverHandler);
 
+  function move() {
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var id = setInterval(frame, 10);
+      function frame() {
+          if (width >= 100) {
+              clearInterval(id);
+          } else {
+              width++;
+              elem.style.width = width + '%';
+          }
+      }
+  }
 
 
 }());
